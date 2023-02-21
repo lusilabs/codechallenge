e17defdc208e9a4856dbfa72b43e1755e809001a
carlo <lobotomisador@gmail.com>
2023-02-21 08:43:42 -0600

    adds lookup and table challenges

---
+++ lookup.js
@@ -0,0 +1,19 @@
+// A column-major data structure is an efficient way of storing tabular data.
+// A typical implementation in javascript can be achieve via an object of arrays e.g.
+
+const table = {
+  name: ['alice', 'bob', 'charly'],
+  age: [25, 50, 30],
+  email: ['alice@gmail.com', null, 'charly@gmail.com']
+}
+
+// A 'lookup function' is the analogue of the the common Array.prototype.find method for tables, that is,
+// given a table and a known (column, value) pair; lookup will return the corresponding value for the desiredKey
+// for example lookup(table, 'name', 'bob', 'age') returns the age of bob which is 50
+// lookup(table, 'email', 'charly@gmail.com', 'name') returns the name that corresponds to the given email, in this case it's 'charly'
+// write a function that implements lookup for an arbitrary table
+// keep in mind your function will be extensively called (for example it could be a part of a library) so caching is a good idea.
+
+export const lookup = (table, column, knownValue, desiredKey) => {
+  /* implement lookup */
+}
